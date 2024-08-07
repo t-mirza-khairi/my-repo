dda112c67577517666769d65cf2db921747ffcdb
t-mirza-khairi <maharleo654@gmail.com>
2024-08-07 21:06:23 +0700

    new commit

--- src/pages/product/[product].tsx
+++ src/pages/product/[product].tsx
@@ -1,23 +1,73 @@
 import { fetcher } from "@/lib/swr/fetcher";
+import { ProductType } from "@/types/product.type";
+import DetailProduct from "@/views/DetailProduct";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 
-const DetailProductPage = () => {
+const DetailProductPage = ({ product }: { product: ProductType }) => {
   const { query } = useRouter();
-
-  const { data, error, isLoading } = useSWR(
-    `/api/product/${query.product}`,
-    fetcher
-  );
-
-  console.log(data);
+  // client side
+  // const { data, error, isLoading } = useSWR(
+  //   `/api/product/${query.product}`,
+  //   fetcher
+  // );
 
   return (
     <div>
-      <h1>Detail Product</h1>
-      <p>Product : {query.product} </p>
+      {/* client side */}
+      {/* <DetailProduct product={isLoading ? [] : data.data} /> */}
+      {/* Server-side & static-side*/}
+      <DetailProduct product={product} />
     </div>
   );
 };
 
 export default DetailProductPage;
+
+// export async function getServerSideProps({
+//   params,
+// }: {
+//   params: { product: string };
+// }) {
+//   //fetch data${}
+//   const res = await fetch(
+//     `http://localhost:3000/api/product/${params.product}`
+//   );
+//   const response = await res.json();
+
+//   return {
+//     props: {
+//       product: response.data,
+//     },
+//   };
+// }
+
+export async function getStaticPaths() {
+  const res = await fetch("http://localhost:3000/api/product");
+  const response = await res.json();
+
+  const paths = response.data.map((product: ProductType) => ({
+    params: {
+      product: product.id,
+    },
+  }));
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({
+  params,
+}: {
+  params: { product: string };
+}) {
+  //fetch data
+  const res = await fetch(
+    `http://localhost:3000/api/product/${params.product}`
+  );
+  const response = await res.json();
+  return {
+    props: {
+      product: response.data,
+    },
+  };
+}
--- src/views/DetailProduct/index.tsx
+++ src/views/DetailProduct/index.tsx
@@ -1,24 +1,26 @@
-import styles from "./DetailProduct.module.scss"
-const DetailProduct = () => {
+import { ProductType } from "@/types/product.type";
+import styles from "./DetailProduct.module.scss";
+const DetailProduct = ({ product }: { product: ProductType }) => {
   return (
-    <div
-
-      className={styles.productDetail}
-    >
-      <div className={styles.productDetail__image}>
-        <img src={product.image} alt={product.name} />
+    <>
+      <h1 className={styles.title}>Detail Product</h1>
+      <div className={styles.productDetail}>
+        <div className={styles.productDetail__image}>
+          <img src={product.image && product.image} alt={product.name} />
+        </div>
+        <h4 className={styles.product__content__item__name}>{product.name}</h4>
+        <p className={styles.product__content__item__category}>
+          {product.category}
+        </p>
+        <p className={styles.product__content__item__price}>
+          {product.price &&
+            product.price.toLocaleString("id-ID", {
+              style: "currency",
+              currency: "IDR",
+            })}
+        </p>
       </div>
-      <h4 className={styles.product__content__item__name}>{product.name}</h4>
-      <p className={styles.product__content__item__category}>
-        {product.category}
-      </p>
-      <p className={styles.product__content__item__price}>
-        {product.price.toLocaleString("id-ID", {
-          style: "currency",
-          currency: "IDR",
-        })}
-      </p>
-    </dic>
+    </>
   );
 };
 
